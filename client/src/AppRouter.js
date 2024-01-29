ff7d4093c110598296709ba1bcef13d294b4a866
Harry0231 <harryrajput544@gmail.com>
2024-01-29 11:51:43 +0530

    update client side file structure

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Add from "./Add";
-import Edit from "./Edit";
-import Login from "./Login";
-import Register from "./Register";
+import App from "./components/App";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import { ToastContainer } from "react-toastify";
-import Profile from "./Profile";
+import Profile from "./components/Profile";
 import "./App.css";
+import Appheader from "./components/Appheader";
 
-import Appheader from "./Appheader";
 const AppRouter = () => {
   return (
     <div className="App">
       <ToastContainer theme="light" position="top-right"></ToastContainer>
 
       <BrowserRouter>
-        <Appheader/>
+        <Appheader />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/create" element={<Add />} />
